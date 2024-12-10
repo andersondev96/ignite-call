4415fbe8189e05461d9ae2ec614c8fce002d7221
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-12-10 20:48:58 -0300

    Deploy do projeto - Ignite Call

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,31 @@
+import '../lib/dayjs'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { SessionProvider } from 'next-auth/react'
+import { DefaultSeo } from 'next-seo'
+import { AppProps } from 'next/app'
+import { queryClient } from '../lib/react-query'
+import { globalStyles } from '../styles/global'
+
+globalStyles()
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://ignite-call.rocketsaet.com.br/',
+            siteName: 'Ignite Call',
+          }}
+        />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
+  )
+}
